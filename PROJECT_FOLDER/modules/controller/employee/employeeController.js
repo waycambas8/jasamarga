ebc842673c9c58b1ef8848a4b8fdf2d66123941d
wahyu fajar wicaksono <waycambas8@gmail.com>
2024-06-05 03:45:33 +0700

    init

--- PROJECT_FOLDER/modules/controller/employee/employeeController.js
+++ PROJECT_FOLDER/modules/controller/employee/employeeController.js
@@ -2,7 +2,6 @@ const { async } = require("rxjs");
 
 class EmployeeController {
     getAllUsers = async (req, res) => {
-        await console.log("testing controller")
         res.json("hallo")
     };
 }
